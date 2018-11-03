f33b616cf49eea3dd0bd0f726a1e94e079148d41
KoEonYack <21300035@handong.edu>
2018-11-03 13:37:27 +0900

    ADD | create week11

---
+++ week11_2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">   
+    <!-- 합쳐지고 최소화된 최신 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- 부가적인 테마 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <br>
+        <p> 본 사이트에 첨부된 사진은 13학번 강민수 학생이 17-2 파이썬 TA세션을 진행하기 위해 제작한 PPT의 일부를 가져왔습니다.</p>
+        <br>
+        <a href="week11_2.html"><img alt="강민수" width="80%" height="80%"  src="https://t1.daumcdn.net/cfile/tistory/9915CC365BDD1EB80A" /></a>
+        <br><br><br>
+        <h3>Let's practice~</h3>
+        <br>
+            <?php
+             echo htmlentities("<p>TA는 잘생겼나요?\n
+             <button type=\"button\" class=\"btn btn-success\">넹~</button>
+             <button type=\"button\" class=\"btn btn-danger\">아녀 ㅡㅡ</button>
+         </p>"); 
+            ?>
+        <br><br>
+        <button type="button" class="btn btn-info" onclick="location.href='week11_3.html' ">코드를 잘 이해했나요??!</button>
+        <br><br><br><br>
+    </div>
+
+    <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
