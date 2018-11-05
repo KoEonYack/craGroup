4e9de51910b6212ecf4a1741bb5ec73c72a6595c
KoEonYack <21300035@handong.edu>
2018-11-05 22:18:39 +0900

    Modify week11

--- week11_2.php
+++ week11_2.php
@@ -14,11 +14,13 @@
 
     <div class="container">
         <br>
-        <p> 본 사이트에 첨부된 사진은 13학번 강민수 학생이 17-2 파이썬 TA세션을 진행하기 위해 제작한 PPT의 일부를 가져왔습니다.</p>
+        <p> 본 사이트에 첨부된 사진은 13학번 강민수 학생이 17-2 파이썬 TA세션을 진행하기 위해 제작한 PPT의 일부를 가져왔습니다. <br>
+        모든 저작권은 강민수 학생에게 있으며, 저작권자의 사용 허락을 받았습니다.
+         </p>
         <br>
-        <a href="week11_2.html"><img alt="강민수" width="80%" height="80%"  src="https://t1.daumcdn.net/cfile/tistory/9915CC365BDD1EB80A" /></a>
+        <a href="error.html"><img alt="강민수" width="80%" height="80%"  src="https://t1.daumcdn.net/cfile/tistory/9915CC365BDD1EB80A" /></a>
         <br><br><br>
-        <h3>Let's practice~</h3>
+        <h3>Let's practice (Web ver)</h3>
         <br>
             <?php
              echo htmlentities("<p>TA는 잘생겼나요?\n
@@ -26,10 +28,13 @@
              <button type=\"button\" class=\"btn btn-danger\">아녀 ㅡㅡ</button>
          </p>"); 
             ?>
+            <hr>
+            <p> Question. 자바스크립트는 어떻게 구성해야할까요? </p>
         <br><br>
         <button type="button" class="btn btn-info" onclick="location.href='week11_3.html' ">코드를 잘 이해했나요??!</button>
         <br><br><br><br>
     </div>
+
 
     <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
